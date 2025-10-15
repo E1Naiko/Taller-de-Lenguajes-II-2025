5865c9d76d02512b807e7a1ee0197aa1d0edfb96
Alam Meza <114436556+Alamm2000py@users.noreply.github.com>
2025-10-15 16:42:25 -0300

    CreandoMetodCargarUsuario

--- proyectojava/src/taller2/plataformatdl2/main.java
+++ proyectojava/src/taller2/plataformatdl2/main.java
@@ -1,7 +1,10 @@
 package taller2.plataformatdl2;
 
 import taller2.DB.DAO.*;
-import taller2.DB.JDBC.*;
+import taller2.DB.JDBC.PeliculasDAOJDBC;
+import taller2.DB.JDBC.ReseniasDAOJDBC;
+import taller2.DB.JDBC.UsuariosFinalDAOJDBC;
+import java.util.Scanner;
 
 /**
  * Clase principal que representa la plataforma de streaming.
@@ -9,7 +12,6 @@ import taller2.DB.JDBC.*;
  * 
  * @author Alam Meza y Nicolas Peñalba
  * @version 1.1 - 2025-10-15
- * 
  */
 
 public class Main {
@@ -22,5 +24,23 @@ public class Main {
         peliculasDAO.crearTablaPeliculas();
         reseniasDAO.crearTablaResenias();
         usuariosFinalDAO.crearTablaUsuarioFinal();
+
+        UsuarioFinal nuevoUsuario= cargarUsuario();
+        usuariosFinalDAO.insertarUsuarioFinal(nuevoUsuario);
+    }
+    //Metodo para registrar Usuario
+    private static UsuarioFinal cargarUsuario(){
+        Scanner scanner= new Scanner(System.in);
+        String nombre= scanner.nextLine();
+        String email = scanner.nextLine();
+        String contrasena = scanner.nextLine();
+        String idioma = scanner.nextLine();
+        String generosPreferidos =scanner.nextLine();
+        String listaPreferida = scanner.nextLine();
+        String historial = scanner.nextLine();
+        UsuarioFinal nuevoUsuario = new UsuarioFinal(nombre,email,contrasena,idioma,generosPreferidos,listaPreferida,historial);
+        System.out.println(nuevoUsuario.toString());
+        scanner.close();
+        return nuevoUsuario;
     }
 }
