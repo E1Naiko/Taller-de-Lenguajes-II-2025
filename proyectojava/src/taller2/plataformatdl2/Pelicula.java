bee01909a9094c38a5dbf93fb3467f4185d106d9
Nicolás Peñalba <matador50200@gmail.com>
2025-10-16 22:07:11 -0300

    Add Pelicula class extending Contenido
    Introduces the Pelicula class in the taller2.plataformatdl2 package, inheriting from Contenido and providing a constructor to initialize quality, audio, and file path.

---
+++ proyectojava/src/taller2/plataformatdl2/Pelicula.java
@@ -0,0 +1,7 @@
+package taller2.plataformatdl2;
+
+public class Pelicula extends Contenido{
+    public Pelicula(String calidad, String audio, String direccionArchivo){
+        super(calidad, audio, direccionArchivo);
+    };
+}
