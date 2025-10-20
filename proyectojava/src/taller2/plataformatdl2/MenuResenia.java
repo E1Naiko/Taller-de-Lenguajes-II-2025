e2f78e22a1ed31236653ae43237f00dc91542d7f
Nicolás Peñalba <matador50200@gmail.com>
2025-10-20 16:52:22 -0300

    Add methods to input and validate Pelicula data
    Introduces methods for loading a Pelicula from user input, validating its fields, and inserting it into the PeliculasDAO. This enhances the MenuResenia class with functionality to add new movies interactively.

--- proyectojava/src/taller2/plataformatdl2/MenuResenia.java
+++ proyectojava/src/taller2/plataformatdl2/MenuResenia.java
@@ -3,6 +3,7 @@ package taller2.plataformatdl2;
 import java.util.Scanner;
 
 import taller2.DB.DAO.FactoryDAO;
+import taller2.plataformatdl2.ManejoDeContenido.Pelicula;
 import taller2.plataformatdl2.ManejoDeUsuarios.UsuarioFinal;
 
 public class MenuResenia {
@@ -13,6 +14,12 @@ public class MenuResenia {
         
         UsuarioFinal nuevoUsuario = cargarUsuario();
         FactoryDAO.getUsuariosFinalDAO().insertarUsuarioFinal(nuevoUsuario);
+    }
+
+    public void cargarPeliculaEnPeliculasDAO(){
+        
+        Pelicula nuevaPelicula = cargarPelicula();
+        FactoryDAO.getPeliculasDAO().insertarPeliculas(nuevaPelicula);
     }
 
     /** 
@@ -43,4 +50,61 @@ public class MenuResenia {
         return nuevoUsuario;
     }
 
+    private Pelicula cargarPelicula(){
+        Scanner scanner= new Scanner(System.in);
+
+        String calidad = null;
+        String audio = null;
+        String direccionArchivo = null;
+
+        do {
+            System.out.println("Ingrese Calidad: ");
+            calidad = scanner.nextLine();
+
+        } while (!verificarCalidad(calidad));
+        
+        do {
+            System.out.println("Ingrese Audio: ");
+            audio = scanner.nextLine();
+
+        } while (!verificarAudio(audio));
+        
+        do {
+            System.out.println("Ingrese Direccion del Archivo: ");
+            direccionArchivo = scanner.nextLine();
+
+        } while (!verificarDireccionArchivo(direccionArchivo));
+        
+        Pelicula nuevaPelicula = new Pelicula(calidad, audio, direccionArchivo);
+        
+        scanner.close();
+        return nuevaPelicula;
+    }
+
+    private boolean verificarCalidad(String calidadIN){ // TODO - Definir criterio para calidad
+        boolean res = true;
+
+        if (!res){
+            System.out.println("ERROR - CALIDAD NO VALIDA");
+        }
+        return res;
+    }
+
+    private boolean verificarAudio(String audioIN){ // TODO - definir criterio para audio
+        boolean res = true;
+
+        if (!res){
+            System.out.println("ERROR - TIPO DE AUDIO NO VALIDO");
+        }
+        return res;
+    }
+    
+    private boolean verificarDireccionArchivo(String direccionArchivoIN){ // TODO - definir criterio para direccion de archivo
+        boolean res = true;
+        
+        if (!res){
+            System.out.println("ERROR - DIRECCION DE ARCHIVO NO VALIDA");
+        }
+        return res;
+    }
 }
