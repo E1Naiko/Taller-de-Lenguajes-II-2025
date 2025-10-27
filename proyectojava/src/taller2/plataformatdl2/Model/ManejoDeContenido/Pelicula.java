86e18660b36c6d6e77755a7731402730b5491f0f
Alam Meza <114436556+Alamm2000py@users.noreply.github.com>
2025-10-27 11:07:28 -0300

    Add user listing option and enhance Pelicula metadata
    Added a new menu option to list users in Main.java and updated the displayed options. Modified Pelicula class to include a Metadatos field, constructor, getter, and setter for handling movie metadata.

--- proyectojava/src/taller2/plataformatdl2/Model/ManejoDeContenido/Pelicula.java
+++ proyectojava/src/taller2/plataformatdl2/Model/ManejoDeContenido/Pelicula.java
@@ -1,7 +1,23 @@
 package taller2.plataformatdl2.Model.ManejoDeContenido;
 
 public class Pelicula extends Contenido{
-    public Pelicula(String calidad, String audio, String direccionArchivo){
+    Metadatos metadatosPelicula;
+
+    public Pelicula(String calidad, String audio, String direccionArchivo, Metadatos metadatosPelicula){
         super(calidad, audio, direccionArchivo);
-    };
+        this.metadatosPelicula= metadatosPelicula;
+    }
+
+    /** 
+     * @return Metadatos
+     */
+    public Metadatos getMetadatos() {
+        return metadatosPelicula;
+    }
+    /** 
+     * @param Metadatos
+     */
+    public void setSinopsis(Metadatos metadatosPelicula) {
+        this.metadatosPelicula = metadatosPelicula;
+    }
 }
