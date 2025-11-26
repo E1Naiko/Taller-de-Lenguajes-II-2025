ae038101f5bc0756fdd667647c7e283c89e963b5
Nicolás Peñalba <matador50200@gmail.com>
2025-11-26 00:59:31 -0300

    Update Main.java

--- proyectojava/src/taller2/plataformatdl2/Main.java
+++ proyectojava/src/taller2/plataformatdl2/Main.java
@@ -19,14 +19,6 @@ public class Main {
     * @param args
     */
     public static void main(String[] args) {
-        new Factory();
-        
-        if (args != null && args.length > 1 && "-AdminMode".equals(args[1])) {
-            MenuResenia menu = new MenuResenia();
-            
-            menu.usarMenuDeConsola();
-        }
-
         try {
             new Factory(); 
             System.out.println("Base de datos conectada joya.");
@@ -35,7 +27,13 @@ public class Main {
             e.printStackTrace();
             return; 
         }
-        
+                
+        if (args != null && args.length > 1 && "-AdminMode".equals(args[1])) {
+            MenuResenia menu = new MenuResenia();
+            
+            menu.usarMenuDeConsola();
+        }
+
         SwingUtilities.invokeLater(() -> {
             try {
                 // Instanciamos la Vista
